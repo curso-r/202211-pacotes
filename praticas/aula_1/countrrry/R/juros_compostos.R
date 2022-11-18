ecbda5169f43dba775cd71d241a9e7f1befb11ff
Beatriz Milz <milz.bea@gmail.com>
2022-11-18 17:52:31 -0300

    adiciona pasta da aula 1

---
+++ praticas/aula_1/countrrry/R/juros_compostos.R
@@ -0,0 +1,17 @@
+# name <- function(variables) {
+#
+# }
+#
+# nome <- function(arg1, arg2, arg3) {
+#   # o que a função faz,
+#   # o que será executado!
+# }
+
+juros_compostos <- function(x, tx = 0.01, tempo = 12){
+  resultado <- x * (1 + tx) ^ tempo
+  resultado
+}
+
+
+
+
